371bf7b77cc4b14340c8a09f0afaedc5a8a7a36f
DC Barringer <nukdcbear@gmail.com>
2020-05-20 16:05:10 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_version = "> 0.12.0"
-  required_providers {
-    aws = "~> 2.62"
-  }
-
-  # backend "s3" {
-  #   bucket  = "dcb-tfe-bootcamp-tfstate"
-  #   key     = "tfstates/pu-app-vpcs"
-  #   region  = "us-east-2"
-  #   encrypt = true
-  # }
-}
 
 provider "aws" {
   alias   = "aws-west"
